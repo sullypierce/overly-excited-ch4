74c630637c559801ed65b63f044d4460f741e335
Sullivan Pierce <sullypierce@gmail.com>
2019-10-10 15:50:26 -0500

    nailed it

--- overly-excited.js
+++ overly-excited.js
@@ -6,7 +6,7 @@ let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement(theWordArray, char, num) {
+ addExcitement= (theWordArray, char, num) => {
 
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
